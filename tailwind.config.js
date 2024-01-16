5069bd06bc74e6f2852e49cf734d198bc53ab1d6
Vishal Ahir <vishalahir8002@gmail.com>
2024-01-16 19:10:23 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {colors: {
+      facebook: {
+        50: '#f0f4f9',
+        100: '#d9e3f1',
+        200: '#b7cbea',
+        300: '#95b3e2',
+        400: '#739bda',
+        500: '#5183d2',
+        600: '#3f6ac9',
+        700: '#2e51bf',
+        800: '#1e38b5',
+        900: '#0f20ab',
+      },
+    },},
+  },
+  plugins: [],
+}
+
